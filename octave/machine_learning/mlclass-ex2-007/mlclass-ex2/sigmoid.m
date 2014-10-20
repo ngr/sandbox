2ffe4b81064a7cccaee98b9da13a9ab8a9361252
Nikolay Grishchenko <grischenko.n@gmail.com>
2014-10-20 23:25:16 +0300

    ML ex-02

--- octave/machine_learning/mlclass-ex2-007/mlclass-ex2/sigmoid.m
+++ octave/machine_learning/mlclass-ex2-007/mlclass-ex2/sigmoid.m
@@ -1,17 +1,37 @@
 function g = sigmoid(z)
-%SIGMOID Compute sigmoid functoon
+%SIGMOID Compute sigmoid function
 %   J = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
-g = zeros(size(z));
+g = zeros(size(z))
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+if size(z, 1) > 1
+%	fprintf('\nz has several rows\n');
 
+	for i = 1:size(z, 1),
+		if size(z, 2) > 1
+			for j = 1:size(z, 2),
+				g(i, j) = 1 / (1 + e ^ -z(i, j));
+			end
+		else
+			g(i) = 1 / (1 + e ^ -z(i));
+		end
+	end
+	
+elseif size(z, 2) > 1
+%	fprintf('\nz has 1 row but multiple columns\n');
+	for j = 1:size(z, 2),
+		g(j) = 1 / (1 + e ^ -z(j));
+	end
 
-
+else
+%	fprintf('\nz is a scalar\n');
+	g = 1 / (1 + e^-z)
+end
 
 % =============================================================
 
