0402a2db31f4c5b1411ac6a8bf710a063cb6ec61
ngr <grischenko.n@gmail.com>
2020-07-14 01:20:20 +0300

    some work

---
+++ python/useless_flights.py
@@ -0,0 +1,122 @@
+from collections import defaultdict
+from typing import List, Dict
+
+
+by_destination = {}
+
+def destinations_dict(schedule) -> Dict:
+    result = defaultdict(list)
+    for flight in schedule:
+        result[flight[1]].append((flight[0], flight[2]))
+    return dict(result)
+
+
+def get_best_price(a, b):
+    global by_destination
+
+    best_price = None
+    for source, price in by_destination[b]:
+        if source == a:
+            if not best_price:
+                best_price = price
+                break
+            if price < best_price:
+                best_price = price
+    else:
+        print(f"not found direct flights {a} to {b}")
+    print(f"best price direct for {a} to {b} is {best_price}")
+    return best_price
+
+
+def get_smart_price(source, destination, best = 1000) -> List:
+
+    global by_destination, root_destination
+
+    best_price = get_best_price(source, destination)
+    # if best_price < best:
+    #     print(f"smart price found direct: {source} to {destination} with best {best_price}")
+    #     return best_price
+    # try:
+    print(f"####Getting price {source} to {destination} with current best {best_price}")
+    for src, value in by_destination[destination]:
+        # src, value = r
+        if source == src:
+            print(f"skipping {src} to {destination}")
+            continue
+
+        # if root_destination == destination:
+        #     print(f"skippin root destination {destination}")
+        #     continue
+
+        # print(src, destination, value)
+        print(f"calculate {src}, {destination}")
+
+        smart_price = get_smart_price(src, destination, best_price)
+
+        # if not smart_price or smart_price == 1000:
+        #     return None
+        if smart_price and smart_price <= best_price:
+            best_price = smart_price
+            break
+        else:
+            return best_price
+    # except ValueError as err:
+    #     print(f"Caught exception for {source} to {destination}: {err}")
+
+    print(f"Best price for {source} -> {destination} is {best_price}")
+
+    return best_price
+
+
+def useless_flight(schedule: List) -> List:
+
+    global by_destination, root_destination
+    by_destination = destinations_dict(schedule)
+    print(by_destination)
+    # t = get_best_price('A', 'C')
+    # print(t)
+
+    result = []
+    for i, (source, destination, cost) in enumerate(schedule):
+        root_destination = destination
+        best_price = cost
+        print("core: ", source, destination, cost)
+
+        if get_best_price(source, destination) < cost:
+            print(f"already in best price {source}, {destination}, {best_price}")
+            result.append(i)
+            continue
+        cost = get_smart_price(source, destination)
+        print(source, destination, cost)
+        if cost < best_price:
+            result.append(i)
+
+# your code here
+    return result
+
+
+if __name__ == '__main__':
+    print("Example:")
+  #   print(useless_flight([['A', 'B', 50],
+  # ['B', 'C', 40],
+  # ['A', 'C', 100]]))
+
+  #   # These "asserts" are used for self-checking and not for an auto-testing
+    print(useless_flight([['A', 'B', 50], ['B', 'C', 30], ['A', 'C', 90], ['A', 'C', 100]]))
+    # print(useless_flight([['A', 'B', 50], ['B', 'C', 30], ['A', 'C', 90]]))
+  #   assert useless_flight([['A', 'B', 50], ['B', 'C', 30], ['A', 'C', 90], ['A', 'C', 100]]) == [2, 3]
+  #   assert useless_flight([['A', 'B', 50],
+  # ['B', 'C', 40],
+  # ['A', 'C', 90]]) == []
+  #   assert useless_flight([['A', 'B', 50],
+  # ['B', 'C', 40],
+  # ['A', 'C', 40]]) == []
+  #   assert useless_flight([['A', 'C', 10],
+  # ['C', 'B', 10],
+  # ['C', 'E', 10],
+  # ['C', 'D', 10],
+  # ['B', 'E', 25],
+  # ['A', 'D', 20],
+  # ['D', 'F', 50],
+  # ['E', 'F', 90]]) == [4, 7]
+  #   print("Coding complete? Click 'Check' to earn cool rewards!")
