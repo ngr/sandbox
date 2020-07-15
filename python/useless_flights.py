79c56f1cc430e8f439d58e10bcd473501ece9731
ngr <grischenko.n@gmail.com>
2020-07-15 21:05:27 +0300

    solve if src and dst are one-way

--- python/useless_flights.py
+++ python/useless_flights.py
@@ -4,6 +4,7 @@ from typing import List, Dict
 
 by_destination = {}
 
+
 def destinations_dict(schedule) -> Dict:
     result = defaultdict(list)
     for flight in schedule:
@@ -15,21 +16,21 @@ def get_best_price(a, b):
     global by_destination
 
     best_price = None
-    for source, price in by_destination[b]:
-        if source == a:
-            if not best_price:
-                best_price = price
-                break
-            if price < best_price:
-                best_price = price
-    else:
-        print(f"not found direct flights {a} to {b}")
-    print(f"best price direct for {a} to {b} is {best_price}")
+    if b in by_destination:
+        for source, price in by_destination[b]:
+            if source == a:
+                if not best_price:
+                    best_price = price
+                    break
+                if price < best_price:
+                    best_price = price
+    # else:
+    #     print(f"not found direct flights {a} to {b}")
+    # print(f"best price direct for {a} to {b} is {best_price}")
     return best_price
 
 
-def get_smart_price(source, destination, best = 1000) -> List:
-
+def get_smart_price(source, destination, exclude_list=[], best=1000) -> List:
     global by_destination, root_destination
 
     best_price = get_best_price(source, destination)
@@ -38,38 +39,48 @@ def get_smart_price(source, destination, best = 1000) -> List:
     #     return best_price
     # try:
     print(f"####Getting price {source} to {destination} with current best {best_price}")
+
+    if destination not in by_destination:
+        return None
+
     for src, value in by_destination[destination]:
+        print(f"You can get to {destination} for {value} from {src}")
+        cost = value
+
         # src, value = r
         if source == src:
             print(f"skipping {src} to {destination}")
             continue
 
+        if (src, destination) in exclude_list:
+            continue
         # if root_destination == destination:
         #     print(f"skippin root destination {destination}")
         #     continue
 
         # print(src, destination, value)
-        print(f"calculate {src}, {destination}")
+        # print(f"calculate {src}, {destination}")
 
-        smart_price = get_smart_price(src, destination, best_price)
+        smart_price = get_smart_price(source, src, exclude_list=[*exclude_list, (src, destination)])
 
         # if not smart_price or smart_price == 1000:
         #     return None
-        if smart_price and smart_price <= best_price:
-            best_price = smart_price
+
+        print(f"you can get to {destination} from {src} for {smart_price}")
+        if smart_price and smart_price + cost <= best_price:
+            best_price = smart_price + cost
             break
         else:
             return best_price
     # except ValueError as err:
     #     print(f"Caught exception for {source} to {destination}: {err}")
 
-    print(f"Best price for {source} -> {destination} is {best_price}")
+    # print(f"Best price for {source} -> {destination} is {best_price}")
 
     return best_price
 
 
 def useless_flight(schedule: List) -> List:
-
     global by_destination, root_destination
     by_destination = destinations_dict(schedule)
     print(by_destination)
@@ -83,40 +94,45 @@ def useless_flight(schedule: List) -> List:
         print("core: ", source, destination, cost)
 
         if get_best_price(source, destination) < cost:
-            print(f"already in best price {source}, {destination}, {best_price}")
+            print(f"already in best price {source}, {destination}, {cost}")
             result.append(i)
             continue
-        cost = get_smart_price(source, destination)
-        print(source, destination, cost)
-        if cost < best_price:
+
+        smart_price = get_smart_price(source, destination)
+        print(source, destination, "cost: ", cost, "smart_price:", smart_price)
+        if smart_price < cost:
+            print(f"smart_price {smart_price} < cost {cost}")
             result.append(i)
 
-# your code here
+    print(f"Result: {result}")
+    # your code here
     return result
 
 
 if __name__ == '__main__':
     print("Example:")
-  #   print(useless_flight([['A', 'B', 50],
-  # ['B', 'C', 40],
-  # ['A', 'C', 100]]))
+    #   print(useless_flight([['A', 'B', 50],
+    # ['B', 'C', 40],
+    # ['A', 'C', 100]]))
 
-  #   # These "asserts" are used for self-checking and not for an auto-testing
-    print(useless_flight([['A', 'B', 50], ['B', 'C', 30], ['A', 'C', 90], ['A', 'C', 100]]))
+    #   # These "asserts" are used for self-checking and not for an auto-testing
+    #   print(useless_flight([['A', 'B', 50], ['B', 'C', 30], ['A', 'C', 90], ['A', 'C', 100]]))
     # print(useless_flight([['A', 'B', 50], ['B', 'C', 30], ['A', 'C', 90]]))
-  #   assert useless_flight([['A', 'B', 50], ['B', 'C', 30], ['A', 'C', 90], ['A', 'C', 100]]) == [2, 3]
-  #   assert useless_flight([['A', 'B', 50],
-  # ['B', 'C', 40],
-  # ['A', 'C', 90]]) == []
-  #   assert useless_flight([['A', 'B', 50],
-  # ['B', 'C', 40],
-  # ['A', 'C', 40]]) == []
-  #   assert useless_flight([['A', 'C', 10],
-  # ['C', 'B', 10],
-  # ['C', 'E', 10],
-  # ['C', 'D', 10],
-  # ['B', 'E', 25],
-  # ['A', 'D', 20],
-  # ['D', 'F', 50],
-  # ['E', 'F', 90]]) == [4, 7]
-  #   print("Coding complete? Click 'Check' to earn cool rewards!")
+
+    # assert useless_flight([['A', 'B', 50], ['B', 'C', 30], ['A', 'C', 90], ['A', 'C', 100]]) == [2, 3]
+    # assert useless_flight([['A', 'B', 50], ['B', 'C', 40], ['A', 'C', 90]]) == []
+
+    #   assert useless_flight([['A', 'B', 50],
+    # ['B', 'C', 40],
+    # ['A', 'C', 40]]) == []
+    assert useless_flight([
+        ['A', 'C', 10],  # 0
+        ['C', 'B', 10],  # 1
+        ['C', 'E', 10],
+        ['C', 'D', 10],  # 3
+        ['B', 'E', 25],  # 4
+        ['A', 'D', 20],
+        ['D', 'F', 50],
+        ['E', 'F', 90]  # 7
+    ]) == [4, 7]
+#   print("Coding complete? Click 'Check' to earn cool rewards!")
